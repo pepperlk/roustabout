f1abdf6d53cb47d48b2a263befee846ae913b365
Lee Pepper <pepperlk@gmail.com>
2015-04-05 21:56:05 -0600

    INIT

---
+++ Roustabout.Tests/Core.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Roustabout.Tests
+{
+    [TestClass]
+    public class Core
+    {
+        [TestMethod]
+        public void CoreInjection()
+        {
+            RB.Register<Class2DepOnClass1>();
+            RB.Register<Class1>();
+            RB.Register<Class3>();
+
+            Class2DepOnClass1 cls2 = RB.Get<Class2DepOnClass1>();
+
+            Assert.IsInstanceOfType(cls2.ijt, typeof(Class1));
+
+
+        }
+
+
+        [TestMethod]
+        public void CoreInjectionStatic()
+        {
+
+            RB.Register<Class1>();
+            RB.Register<Class3>();
+            RB.Register<Class2DepOnClass1>(true);
+
+            Class2DepOnClass1 cls2 = RB.Get<Class2DepOnClass1>();
+
+            Assert.IsInstanceOfType(cls2.ijt, typeof(Class1));
+
+
+        }
+
+
+        [TestMethod]
+        public void AutoInjection()
+        {
+       
+            Class2DepOnClass1 cls2 = RB.Get<Class2DepOnClass1>();
+
+            Assert.IsInstanceOfType(cls2.ijt, typeof(Class1));
+        }
+    }
+
+
+    public class Class2DepOnClass1
+    {
+        public Class2DepOnClass1(Class1 cls, Class3 three)
+        {
+            ijt = cls;
+        }
+
+        public object ijt { get; set; }
+    }
+
+    public class Class1
+    {
+
+    }
+
+    public class Class3
+    {
+        public Class3(Class1 cls)
+        {
+            ijt = cls;
+        }
+
+        public object ijt { get; set; }
+    }
+
+}
---
+++ Roustabout/RB.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Roustabout
+{
+    public class RB
+    {
+
+        private static Dictionary<Type, object> _cache = new Dictionary<Type, object>();
+
+        public static void Register<T>(bool keepstatic = false)
+        {
+            Register(typeof(T), keepstatic);
+        }
+
+        public static void Register(Type t, bool keepstatic = false)
+        {
+            object obj = null;
+            if (keepstatic)
+            {
+                _cache[t] = null;
+                obj = Get(t);
+            }
+
+            _cache[t] = obj;
+
+        }
+
+        public static T Get<T>()
+        {
+            return (T)Get(typeof(T));
+        }
+
+        public static object Get(Type t)
+        {
+
+            if (!_cache.ContainsKey(t))
+            {
+
+                Register(t);
+
+            }
+
+                if (_cache[t] != null)
+                {
+                    return _cache[t];
+                }
+
+                var ctor = t.GetConstructors().FirstOrDefault();
+                var cparams = ctor.GetParameters();
+
+                if (cparams.Length == 0)
+                {
+                    return ctor.Invoke(new object[] { });
+
+                    //return Activator.CreateInstance(t);
+                }
+                else
+                {
+                    
+                    
+                    var tl = new List<object>();
+                    foreach (var pt in cparams)
+                    {
+                        tl.Add(RB.Get(pt.ParameterType));
+                    }
+
+
+                    return ctor.Invoke(tl.ToArray());
+
+                }
+
+
+
+
+            
+
+            Debug.WriteLine(string.Format("({0}) is not registered for injection", t.Name));
+            throw new Exception(string.Format("Type not registered with RB use RB.Register<{0}>()", t.Name));
+
+
+
+        }
+
+        
+    }
+}
